e6227ecc86c0f61c89858b99194f93148dbea1a3
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-16 14:28:45 +0900

    3-view engine

--- Exepress/hello-express/routes/admin.js
+++ Exepress/hello-express/routes/admin.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const nunjucks = require("nunjucks");
 
 router.get("/", (req, res) => {
     res.send("admin 이후 url");
 });
 
 router.get("/products", (req, res) => {
-    res.send("admin products url");
+    // res.send("admin products url");
+    res.render("admin/products.html", {
+        message: "hello!!!!!",
+        online: "express",
+    });
 });
 
 module.exports = router;
