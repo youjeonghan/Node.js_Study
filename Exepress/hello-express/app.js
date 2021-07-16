8edad6aea1eca4501fb503fccc38fb8ec5caaf28
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-16 14:57:52 +0900

    3-미들웨어

--- Exepress/hello-express/app.js
+++ Exepress/hello-express/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const logger = require("morgan");
 
 const admin = require("./routes/admin");
 
@@ -11,11 +12,19 @@ nunjucks.configure("template", {
     express: app,
 });
 
+// 미들웨어 셋팅
+app.use(logger("dev"));
+
 app.get("/", (req, res) => {
     res.send("hello express");
 });
 
-app.use("/admin", admin);
+function vipMiddleware(req, res, next) {
+    console.log("최우선 미들웨어");
+    next();
+}
+
+app.use("/admin", vipMiddleware, admin);
 
 app.listen(port, () => {
     console.log("express listening on port", port);
