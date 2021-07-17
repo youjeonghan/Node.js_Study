0621241acf4ca2441e171360543343d243207cef
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-17 14:15:25 +0900

    Ch 02. 든든한 개발 환경 설정하기_05. TypeScript로 타입 에러 체크하기

--- 백엔드_part1/main.js
+++ 백엔드_part1/main.js
@@ -1,3 +1,8 @@
-// Formatting, Linting
+// @ts-check
+// Formatting, Linting, Type checking
 // Formatting: Prettier
 // Linting: ESLint
+
+const someString = "hello"
+const result = Math.log(someString)
+console.log(result)
