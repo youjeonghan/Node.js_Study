c741819f4045ce2be1aca153440d4a5575929335
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-17 14:33:44 +0900

    Ch 02. 든든한 개발 환경 설정하기_06. node 환경에서 TypeScript 도움 받기

--- 백엔드_part1/main.js
+++ 백엔드_part1/main.js
@@ -2,7 +2,16 @@
 // Formatting, Linting, Type checking
 // Formatting: Prettier
 // Linting: ESLint
+// Type checking: Typescript
 
-const someString = "hello"
-const result = Math.log(someString)
-console.log(result)
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.end("hello!")
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}.`)
+})
